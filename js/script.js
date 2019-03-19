9e35b68036465db2b765feb4ea88857fca65d8eb
sergeymaksimenko <srgmaksimenko@gmail.com>
2019-03-20 00:25:35 +0200

    added button disappearance code refactor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-document.querySelector('.table').onmousemove = function (event) {
-    let target = event.target;
-    let buttonMinusColumn = document.querySelector('.buttonMinusColumn');
-    let buttonMinusRow = document.querySelector('.buttonMinusRow');
-    let table = document.querySelector('.table');
-    buttonMinusColumn.style.marginLeft = event.pageX - table.offsetLeft + (target.offsetWidth / 2) + 'px';
-    buttonMinusRow.style.marginTop = event.pageY - table.offsetTop + (target.offsetHeight / 2) + 'px';
+window.onload = function () {
+    let cellElements = document.getElementsByClassName('cell');
+    Array.prototype.forEach.call(cellElements, function (element) {
+        element.addEventListener('mousemove', function () {
+            moveButtons(element)
+        }, false);
+        element.addEventListener('mouseout', hideButtons, false);
+    });
 };
 
+function moveButtons(element) {
+    let buttonMinusColumn = document.querySelector('.buttonMinusColumn');
+    let buttonMinusRow = document.querySelector('.buttonMinusRow');
+    buttonMinusColumn.style.visibility = 'visible';
+    buttonMinusRow.style.visibility = 'visible';
+    let table = document.querySelector('.table');
+    buttonMinusColumn.style.marginLeft = event.pageX - table.offsetLeft + (element.offsetWidth / 2) + 'px';
+    buttonMinusRow.style.marginTop = event.pageY - table.offsetTop - (element.offsetHeight / 2) + 'px';
+}
+
+function hideButtons(event) {
+    if (!event.relatedTarget.classList.contains('cell')) {
+        let buttonMinusColumn = document.querySelector('.buttonMinusColumn');
+        let buttonMinusRow = document.querySelector('.buttonMinusRow');
+        buttonMinusColumn.style.visibility = 'hidden';
+        buttonMinusRow.style.visibility = 'hidden';
+    }
+}
